34e10db5edee776a11002aea8fbf7218cc2abb63
junghongseop <a01082372487@gmail.com>
2024-04-02 20:14:00 +0900

    add :: 애니메이션 추가 및 도형 추가

--- src/practice.js
+++ src/practice.js
@@ -21,20 +21,30 @@ if (WEBGL.isWebGLAvailable()) {
   document.body.appendChild(renderer.domElement);
 
   // 매쉬
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({ color: 0x999999 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const geometry01 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+  const material01 = new THREE.MeshStandardMaterial({ color: 0x999999 });
+  const obj01 = new THREE.Mesh(geometry01, material01);
+  obj01.position.x = 0.5;
+  scene.add(obj01);
+
+  const geometry02 = new THREE.IcosahedronGeometry(0.3, 0);
+  const material02 = new THREE.MeshStandardMaterial({ color: 0x999999 });
+  const obj02 = new THREE.Mesh(geometry02, material02);
+  obj02.position.x = -0.5;
+  scene.add(obj02);
 
   // renderer.render(scene, camera);
 
   // 애니메이션 적용
   function render(time) {
-    time *= 0.001;
+    time *= 0.0005;  // 회전 시간 속도
 
     // 도형이 회전하도록
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    // cube.rotation.x = time;
+    obj01.rotation.y = time;
+
+    obj02.rotation.x = time;
+    obj02.rotation.y = time;
 
     renderer.render(scene, camera);
 
